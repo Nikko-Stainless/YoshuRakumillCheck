b5473a5334c7dd55a861d4b6a3fbb2d0b10942bb
l.linh <l.linh@WS104>
2024-09-03 16:39:07 +0900

    完了

--- YoshuRakumillCheck/Form1.Designer.vb
+++ YoshuRakumillCheck/Form1.Designer.vb
@@ -22,17 +22,122 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(1, 2)
+        Me.TabControl1.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1096, 740)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.DataGridView1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 32)
+        Me.TabPage1.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.TabPage1.Size = New System.Drawing.Size(1088, 704)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "楽ミルデータチェック"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.Control
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(4, 0)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersVisible = False
+        Me.DataGridView1.RowHeadersWidth = 32
+        DataGridViewCellStyle3.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.DataGridView1.RowsDefaultCellStyle = DataGridViewCellStyle3
+        Me.DataGridView1.RowTemplate.Height = 21
+        Me.DataGridView1.Size = New System.Drawing.Size(1084, 704)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.DataGridView2)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 32)
+        Me.TabPage2.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.TabPage2.Size = New System.Drawing.Size(1088, 704)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "楽ミル当日登録データ"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.AllowUserToAddRows = False
+        Me.DataGridView2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView2.BackgroundColor = System.Drawing.SystemColors.Control
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(2, 0)
+        Me.DataGridView2.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.ReadOnly = True
+        Me.DataGridView2.RowHeadersVisible = False
+        Me.DataGridView2.RowHeadersWidth = 35
+        DataGridViewCellStyle4.Font = New System.Drawing.Font("メイリオ", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.DataGridView2.RowsDefaultCellStyle = DataGridViewCellStyle4
+        Me.DataGridView2.RowTemplate.Height = 21
+        Me.DataGridView2.Size = New System.Drawing.Size(1084, 704)
+        Me.DataGridView2.TabIndex = 1
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 23.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(717, 405)
+        Me.ClientSize = New System.Drawing.Size(1098, 743)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Font = New System.Drawing.Font("メイリオ", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage2.ResumeLayout(False)
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents DataGridView2 As DataGridView
 End Class
--- YoshuRakumillCheck/Form1.vb
+++ YoshuRakumillCheck/Form1.vb
@@ -1,3 +1,146 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports Oracle.ManagedDataAccess.Client
+Public Class Form1
+    Dim folderPath As String = ""
+    Dim listKeys As String = ""
+    Dim connectionString As String = ""
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Not LoadFileini() Then Me.Close()
+
+        SelectedIndexChanged()
+    End Sub
+    'load file ini 
+    Private Function LoadFileini() As Boolean
+        Dim file = Application.StartupPath + "\YoshuRakumillCheck_AppSettings.ini"
+
+        If Not IO.File.Exists(file) Then Return False
+
+        Dim lines As String() = IO.File.ReadAllLines(file)
+        Dim section As String = ""
+
+        For Each line As String In lines
+            line = line.Trim()
+            If line.StartsWith("[") AndAlso line.EndsWith("]") Then
+                section = line.Trim("["c, "]"c)
+            Else
+                Select Case section
+                    Case "ConnectString"
+                        connectionString = line.Trim()
+                    Case "RakumillCheckFolder"
+                        folderPath = line.Trim()
+                End Select
+            End If
+        Next
+        Return True
+    End Function
+    'SQL
+    Private Function GetData(query As String) As DataTable
+        Dim dataTable As New DataTable()
+
+        Using connection As New OracleConnection(connectionString)
+            Using command As New OracleCommand(query, connection)
+                Using adapter As New OracleDataAdapter(command)
+                    connection.Open()
+                    adapter.Fill(dataTable)
+                End Using
+            End Using
+        End Using
+
+        Return dataTable
+    End Function
+    Private Sub SelectedIndexChanged() Handles TabControl1.SelectedIndexChanged
+        Dim selectedIndex As Integer = TabControl1.SelectedIndex
+        Select Case selectedIndex
+            Case 0
+                selectDataTab1()
+            Case 1
+                selectDataTab2()
+        End Select
+    End Sub
+
+#Region "tab_1"
+    Dim sql_TAB1 = "
+    SELECT a.URIKEIYAKUNO as 売契約No, 
+           a.URIKEIYAKUGYOUNO as 行No,
+           a.sunpou as 寸法,
+           a.insuu as 員数,
+           a.shukkaymd as 出荷日,
+           a.W_NO , a.C_NO, 
+           a.milsheetno as ミルシートNo,
+           a.MAKERCD as メーカーCD,
+           b.makerryakumei as メーカー略称,
+           b.makermei as メーカー名
+    FROM 
+          RKM_shipping a, rkm_m_maker b 
+   WHERE 
+          a.makercd = b.makercd(+) AND a.URIKEIYAKUNO IN ({0}) 
+   ORDER BY 
+          1,2"
+    Private Sub selectDataTab1()
+        Dim pdfFiles As String() = Directory.GetFiles(folderPath, "*.pdf")
+
+        For Each pdfFile As String In pdfFiles
+            Dim fileName As String = Path.GetFileNameWithoutExtension(pdfFile)
+            If fileName.Length >= 8 Then
+                Dim first8Chars As String = fileName.Substring(0, 8)
+                ' ファイル名の最初の8文字を取得
+                If IsNumeric(first8Chars) Then
+                    ' 最初の8文字が数字であるかを確認
+                    listKeys = listKeys + "'" + first8Chars + "',"
+                End If
+            End If
+        Next
+
+        If listKeys.Length > 0 Then
+            listKeys = listKeys.TrimEnd(","c)
+        End If
+
+        If Not String.IsNullOrEmpty(listKeys) Then
+            Dim sqlQuery As String = String.Format(sql_TAB1, listKeys)
+            Dim resultTable As DataTable = GetData(sqlQuery)
+            DataGridView1.DataSource = Nothing
+            DataGridView1.DataSource = resultTable
+        End If
+    End Sub
+
+#End Region
+#Region "tab_2"
+    Dim sql_TAB2 = "
+    SELECT
+    distinct d.KAISHAKBN as 会社区分,
+　　d.shimeidisp as 締切　,
+　　decode(lengthb(a.tourokutime),7,0||substr( a.tourokutime,1,1) || ':' || substr(a.tourokutime, 2, 2),substr(a.tourokutime,1,2) || ':' || substr(a.tourokutime, 3, 2) ) as 時間,
+　　a.USER_YOBI1 as orderNo,
+　　a.sunpou as 寸法,
+　　a.inzuu as 員数　,
+　　a.MAKERCD as メーカーCD　,
+　　b.MAKERRYAKUMEI as メーカー略称　,
+　　a.c_no　,
+　　a.MILSHEETNO as ミルシートNo　,
+　　decode(c.MILSHEET_YOUHIKBN,1,'要','不要') as 区分,
+　　c.SHUKKAYMD as 出荷日,
+　　c.SHUKKAYMD2 as 出荷日２,
+　　c.TOUROKUYMD as 出荷データ登録日,
+　　c.KOUSHINKAISHAKBN as 更新区分　,
+　　c.KOUSHINtantouno as 更新担当
+
+FROM
+  RKM_milsheet a,rkm_shipping c,rkm_m_shain d,
+   rkm_m_maker b
+where a.USER_YOBI1=c.URIKEIYAKUNO and c.KOUSHINTANTOUNO=d.tantouno and
+ a.TOUROKUYMD=to_char(sysdate,'yyyymmdd') and a.MAKERCD=b.MAKERCD
+order by
+  1,2,3"
+
+    Private Sub selectDataTab2()
+        Dim resultTable As DataTable = GetData(sql_TAB2)
+        DataGridView2.DataSource = Nothing
+        DataGridView2.DataSource = resultTable
+    End Sub
+#End Region
+
+
+
 
 End Class
